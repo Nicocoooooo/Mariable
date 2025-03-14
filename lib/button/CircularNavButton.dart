f260ff0f6008a15cbf5901309f5c6ee637b432e9
eliiinzooo <elise.collignon@edu.ece.fr>
2025-03-14 11:31:15 +0100

    version precedente problème

---
+++ lib/button/CircularNavButton.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class CircularNavButton extends StatefulWidget {
+  final IconData icon;
+  final VoidCallback onPressed;
+  final Color backgroundColor;
+  final Color iconColor;
+
+  const CircularNavButton({
+    Key? key,
+    required this.icon,
+    required this.onPressed,
+    this.backgroundColor = Colors.white,
+    this.iconColor = const Color(0xFF524B46), // Couleur accent par défaut
+  }) : super(key: key);
+
+  @override
+  State<CircularNavButton> createState() => _CircularNavButtonState();
+}
+
+class _CircularNavButtonState extends State<CircularNavButton> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+  bool _isPressed = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 150),
+      vsync: this,
+    );
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 0.95).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _handleTapDown(TapDownDetails details) {
+    setState(() {
+      _isPressed = true;
+    });
+    _controller.forward();
+  }
+
+  void _handleTapUp(TapUpDetails details) {
+    setState(() {
+      _isPressed = false;
+    });
+    _controller.reverse();
+  }
+
+  void _handleTapCancel() {
+    setState(() {
+      _isPressed = false;
+    });
+    _controller.reverse();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTapDown: _handleTapDown,
+      onTapUp: _handleTapUp,
+      onTapCancel: _handleTapCancel,
+      onTap: widget.onPressed,
+      child: AnimatedBuilder(
+        animation: _controller,
+        builder: (context, child) {
+          return Transform.scale(
+            scale: _scaleAnimation.value,
+            child: Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                color: widget.backgroundColor,
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(_isPressed ? 0.1 : 0.2),
+                    blurRadius: _isPressed ? 3 : 6,
+                    offset: Offset(0, _isPressed ? 1 : 2),
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Icon(
+                  widget.icon,
+                  color: widget.iconColor,
+                  size: 20,
+                ),
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
